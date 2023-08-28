cbc7552cbe1e1625cfb2d972df24bcbf34a80f30
TanishqSingla <tanishqsingla00@gmail.com>
2023-08-28 12:12:25 +0530

    feat: Update regex for getPath recognize more patterns
    Add tests for getPath

--- utils/getPath.ts
+++ utils/getPath.ts
@@ -1,9 +1,9 @@
 export const getPath = (url: string): string => {
-	const path = url.match(/http[s]?:\/\/[a-zA-Z:-]+\d+?\/?([a-zA-Z0-9/]+)/);
+	const path = url.match(/https?:\/\/[a-zA-Z0-9-.]+\w+\:?\d*(\/[a-zA-Z0-9%-\/]+)/);
 
-	if(path && path[3]) {
-		return path[3];
+	if(path && path[1]) {
+		return path[1];
 	}
 
-	return "";
+	return "/";
 }
